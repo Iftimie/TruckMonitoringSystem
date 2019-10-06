5469805356f920922474b027644cf541c9a9428f
Iftimie Florentin Alexandru <iftimie.alexandru.florentin@gmail.com>
2019-10-06 11:30:08 +0300

    added video results interface

--- truckms/service/service.py
+++ truckms/service/service.py
@@ -8,6 +8,10 @@ from truckms.inference.utils import image_generator
 from flask import Flask, render_template, send_from_directory, make_response, request, redirect, url_for, session
 import os.path as osp
 from flask_bootstrap import Bootstrap
+from io import BytesIO
+import base64
+import cv2
+import io
 
 
 def analyze_movie(video_path, max_operating_res):
@@ -35,15 +39,13 @@ def create_microservice(upload_directory="tms_upload_dir", num_workers=1, max_op
 
     @app.route("/upload_recordings", methods=['POST'])
     def upload_recordings():
-
-        files_dict = {}
         for filename in request.files:
             f = request.files[filename]
             filename = secure_filename(filename)
             filepath = os.path.join(upload_directory, filename)
-            files_dict[filename] = filepath
             f.save(filepath)
             app.worker_pool.apply_async(func=analyze_movie, args=(filepath, max_operating_res))
+            app.logger.info('started this shit')
 
         return redirect(url_for("index"))
 
@@ -55,12 +57,32 @@ def create_microservice(upload_directory="tms_upload_dir", num_workers=1, max_op
 
     @app.route('/check_status')
     def check_status_menu():
-        video_items = [{'filename':'spanac.avi', 'status': 'true'},
-                       {'filename': 'wtf.avi', 'status': 'false'}]
+        video_items = []
+        for file in filter(lambda x: '.csv' not in x, os.listdir(upload_directory)):
+
+            video_items.append({'filename': file,
+                                'status': 'ready' if osp.exists(osp.join(upload_directory, os.path.splitext(file)[0]+'.csv')) else 'processing'})
         partial_destination_url = '/show_video?filename='
         resp = make_response(render_template("check_status.html", partial_destination_url=partial_destination_url,
                                              video_items=video_items))
         return resp
+
+
+    @app.route('/show_video')
+    def show_video():
+        filename = request.args.get('filename')
+
+        image = cv2.imread(osp.join(osp.dirname(__file__), 'templates', 'assets', '32624372793_1fe69d0349_k.jpg'))
+        is_success, buffer = cv2.imencode(".jpg", image)
+        io_buf = io.BytesIO(buffer)
+
+
+        figdata_png = base64.b64encode(io_buf.getvalue())
+        result = str(figdata_png)[2:-1]
+
+        resp = make_response(render_template("show_video.html", result=result))
+        return resp
+
 
     @app.route('/')
     def root():
