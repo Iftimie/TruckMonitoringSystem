7699beb7a4e80333d7ea1da6486e69890813aae7
Iftimie Florentin Alexandru <iftimie.alexandru.florentin@gmail.com>
2019-10-18 10:35:33 +0300

    Update download_live_camera.py

--- download_live_camera.py
+++ download_live_camera.py
@@ -1,6 +1,9 @@
 import subprocess
 import time
 import traceback
+import os
+from subprocess import Popen, PIPE, STDOUT
+import signal
 
 sleep_time = 3600
 url = "http://178.239.225.102/axis-cgi/mjpg/video.cgi"
@@ -8,14 +11,19 @@ url = "http://178.239.225.102/axis-cgi/mjpg/video.cgi"
 i = 0
 while True:
 	try:
-		proc = subprocess.Popen(["ffmpeg", "-i", url, "output_"+str(i)+".mp4"], stdin=subprocess.PIPE,
-		                            stdout=subprocess.PIPE,
-		                            stderr=subprocess.PIPE)
+		proc = subprocess.Popen(' '.join(["ffmpeg", "-i", url, "-t", "01:00:00.000" , "output_"+str(i)+".mp4"]), shell=True, stdin=PIPE, stdout=PIPE)
 		time.sleep(sleep_time)
-		print ("done waiting")
-		proc.communicate(b'q\n')
-		proc.wait()
-		print ("done")
+		for j in range(4):
+			try:
+				proc.communicate(input=b'q')
+			except:
+				traceback.print_exc()
+				pass
+			finally:
+				time.sleep(1)
+		if proc.poll() is None:
+			os.kill(proc.pid, signal.SIGTERM)
+			print ("Done with one video")
 	except:
 		traceback.print_exc()
 		continue
