ca2b0377ef8edd254fe819199c239cb9936a88f9
Iftimie Florentin Alexandru <iftimie.alexandru.florentin@gmail.com>
2019-11-07 01:32:27 +0200

    started working on communication

--- bookkeeper/service.py
+++ bookkeeper/service.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, make_response, request, redirect, url_for
-from werkzeug import secure_filename
-
+from flask import Flask, make_response, request, jsonify
+from collections import namedtuple
 
 def shutdown():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -10,19 +9,23 @@ def shutdown():
     return 'Server shutting down...'
 
 
+NodeState = namedtuple('NodeState', ['ip', 'port', 'workload', 'hardware', 'nickname', 'node_type', 'email'])
+set_states = set()  # TODO replace with a local syncronized database
 def node_states():
-    content = request.json
-    state = {"ip": request.environ['REMOTE_ADDR'],
-             "port": request.environ['REMOTE_PORT']}
-    print (content)
-    return make_response("done", 200)
+    if request.method == 'POST':
+        content = request.json
+        content['ip'] = request.environ['REMOTE_ADDR']
+        set_states.add(NodeState(**content))
+        return make_response("done", 200)
+    else:
+        return jsonify(list(set_states))
 
 def create_microservice(debug=False):
     app = Flask(__name__)
 
     if debug:
         app.route('/shutdown', methods=['POST'])(shutdown)
-    app.route("/node_states", methods=['POST'])(node_states)
+    app.route("/node_states", methods=['POST', 'GET'])(node_states)
 
 
 
--- tests/bookkeeper/test_servic.py
+++ tests/bookkeeper/test_servic.py
@@ -1,20 +1,9 @@
 from bookkeeper.service import create_microservice
 import requests
-
-def test_create_microservice():
-
-
-    app = create_microservice()
-
-    client = app.test_client()
-    # aau-rainsnow dataset. found on kaggle
-    data = {'cevaa': 'cevaaadata'}
-
-    res = client.post("/node_states", json=data)
-    assert (res.status_code == 200) # 302 is found redirect
-
 from werkzeug.serving import make_server
 import threading
+
+
 class ServerThread(threading.Thread):
 
     def __init__(self, app, host='127.0.0.1', port=5000):
@@ -29,10 +18,40 @@ class ServerThread(threading.Thread):
     def shutdown(self):
         self.srv.shutdown()
 
+
 def test_method_of_service():
     app = create_microservice(debug=True)
 
-    server = ServerThread(app)
+    current_port = 5000
+    server = ServerThread(app, port=current_port)
     server.start()
-    requests.post('http://localhost:5000/node_states')
+    data = {'port': current_port,
+            'workload': 0,
+            'hardware': "Nvidia GTX 960M Intel i7",
+            'nickname': "rmstn",
+            'node_type': "worker",
+            'email': 'iftimie.alexandru.florentin@gmail.com'}
+    res = requests.post('http://localhost:5000/node_states', json=data)
+    assert (res.status_code == 200)
+    res = requests.get('http://localhost:5000/node_states').json()[0]  # will get the data defined above
+    del res['ip']
+    assert res == data
     server.shutdown()
+
+
+def test_two_services():
+    server1 = ServerThread(create_microservice(debug=True), port=5000)
+    server2 = ServerThread(create_microservice(debug=True), port=5001)
+    server1.start()
+    server2.start()
+    data = {'port': 5000, 'workload': 0, 'hardware': "Nvidia GTX 960M Intel i7", 'nickname': "rmstn",
+            'node_type': "worker", 'email': 'iftimie.alexandru.florentin@gmail.com'}
+    res = requests.post('http://localhost:5000/node_states', json=data)
+    data = {'port': 5001, 'workload': 0, 'hardware': "Nvidia GTX 960M Intel i7", 'nickname': "rmstn",
+            'node_type': "worker", 'email': 'iftimie.alexandru.florentin@gmail.com'}
+    res = requests.post('http://localhost:5000/node_states', json=data)
+
+    res = requests.get('http://localhost:5000/node_states').json()  # will get the data defined above
+    assert len(res) == 2
+    server1.shutdown()
+    server2.shutdown()
