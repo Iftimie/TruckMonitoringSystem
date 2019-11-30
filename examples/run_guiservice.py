ff01303501ae25b9e04ff568ae64e96600e82ba9
Alexandru.Iftimie@xperi.com <alexandru.iftimie@xperi.com>
2019-11-30 15:28:44 +0200

    Update run_guiservice.py

--- examples/run_guiservice.py
+++ examples/run_guiservice.py
@@ -8,7 +8,7 @@ def flaskuimain():
     # https://github.com/ClimenteA/flaskwebgui
 
     db_url = 'sqlite:///' + r'D:\tms_data\guiservice\database.sqlite'
-    remove_db = True
+    remove_db = False
     if remove_db and os.path.exists(db_url.replace('sqlite:///', '')):
         os.remove(db_url.replace('sqlite:///', ''))
 
@@ -17,7 +17,7 @@ def flaskuimain():
     work_func, work_pool, list_futures = get_job_dispathcher(db_url=db_url, num_workers=1, max_operating_res=320, skip=0, local_port=port)
     uiapp, app = create_guiservice(db_url, work_func, port)
 
-    bookkeeper_bp = create_bookkeeper_p2pblueprint(local_port=port, app_roles=[], discovery_ips_file="discovery_ips")
+    bookkeeper_bp = create_bookkeeper_p2pblueprint(local_port=port, app_roles=app.roles, discovery_ips_file="discovery_ips")
     app.register_blueprint(bookkeeper_bp)
 
     uiapp.run()
